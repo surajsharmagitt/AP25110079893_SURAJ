66a1921488b0a10af0d614c5666399f3d6e45503
Suraj Sharma <sachinsharma91829@gmail.com>
2026-04-23 09:56:28 +0530

    Refactor expression conversion functions and variables

--- labfinal/lab5/expression_conversion.c
+++ labfinal/lab5/expression_conversion.c
@@ -1,104 +1,75 @@
-// Expression conversion including infix to prefix and infix to postfix.
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <ctype.h>
+#include <string.h>
 
-#define MAX 100
-int getPriority(char C)
+#define SIZE 100
+
+char stack[SIZE];
+int top = -1;
+
+void push(char x)
 {
-    if (C == '-' || C == '+')
+    stack[++top] = x;
+}
+
+char pop()
+{
+    return stack[top--];
+}
+
+char peek()
+{
+    return stack[top];
+}
+
+int precedence(char x)
+{
+    if(x == '+' || x == '-')
         return 1;
-    else if (C == '*' || C == '/')
+    if(x == '*' || x == '/')
         return 2;
-    else if (C == '^')
+    if(x == '^')
         return 3;
     return 0;
 }
-void reverse(char *exp)
-{
-    int i, j;
-    char temp;
-    for (i = 0, j = strlen(exp) - 1; i < j; i++, j--)
-    {
-        temp = exp[i];
-        exp[i] = exp[j];
-        exp[j] = temp;
-    }
-}
-void infixToPostfix(char infix[], char postfix[])
-{
-    char stack[MAX];
-    int top = -1;
-    int i = 0, j = 0;
-    for (i = 0; infix[i] != '\0'; i++)
-    {
-        if (isalnum(infix[i]))
-        {
-            postfix[j++] = infix[i];
-        }
-        else if (infix[i] == '(')
-        {
-            stack[++top] = infix[i];
-        }
-        else if (infix[i] == ')')
-        {
-            while (top != -1 && stack[top] != '(')
-            {
-                postfix[j++] = stack[top--];
-            }
-            if (top != -1)
-                top--;
-        }
-        else
-        {
-            while (top != -1 && getPriority(stack[top]) >= getPriority(infix[i]))
-            {
-                postfix[j++] = stack[top--];
-            }
-            stack[++top] = infix[i];
-        }
-    }
-    while (top != -1)
-    {
-        postfix[j++] = stack[top--];
-    }
-    postfix[j] = '\0';
-}
-void infixToPrefix(char infix[], char prefix[])
-{
-    char tempInfix[MAX];
-    strcpy(tempInfix, infix);
 
-    reverse(tempInfix);
-    for (int i = 0; tempInfix[i] != '\0'; i++)
-    {
-        if (tempInfix[i] == '(')
-        {
-            tempInfix[i] = ')';
-        }
-        else if (tempInfix[i] == ')')
-        {
-            tempInfix[i] = '(';
-        }
-    }
-    char postfix[MAX];
-    infixToPostfix(tempInfix, postfix);
-
-    reverse(postfix);
-    strcpy(prefix, postfix);
-}
 int main()
 {
-    char infix[MAX], postfix[MAX], prefix[MAX];
+    char infix[100], postfix[100];
+    int i, j = 0;
 
-    printf("Enter Infix Expression: ");
+    printf("Enter infix expression: ");
     scanf("%s", infix);
 
-    infixToPostfix(infix, postfix);
-    printf("Postfix Expression: %s\n", postfix);
+    for(i = 0; infix[i] != '\0'; i++)
+    {
+        if(isalnum(infix[i]))
+            postfix[j++] = infix[i];
 
-    infixToPrefix(infix, prefix);
-    printf("Prefix Expression: %s\n", prefix);
+        else if(infix[i] == '(')
+            push(infix[i]);
+
+        else if(infix[i] == ')')
+        {
+            while(top != -1 && peek() != '(')
+                postfix[j++] = pop();
+            pop();
+        }
+
+        else
+        {
+            while(top != -1 && precedence(peek()) >= precedence(infix[i]))
+                postfix[j++] = pop();
+            push(infix[i]);
+        }
+    }
+
+    while(top != -1)
+        postfix[j++] = pop();
+
+    postfix[j] = '\0';
+
+    printf("Postfix expression: %s\n", postfix);
+
     return 0;
 }
